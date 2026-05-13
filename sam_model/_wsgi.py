30db5bf6838100a0965862865f8fd18071a0cb73
Swarup Shekhar <shekharswarup67@gmail.com>
2026-05-13 15:46:21 +0530

    fix(sam): add __name__ == '__main__' block to _wsgi.py to keep server running

--- sam_model/_wsgi.py
+++ sam_model/_wsgi.py
@@ -8,3 +8,6 @@ from label_studio_ml.api import init_app
 from model import SamMobileSegmentation
 
 app = init_app(SamMobileSegmentation)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9090)
